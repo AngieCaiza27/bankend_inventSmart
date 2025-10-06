812965729de288dd9fc92a4179c255879dec755d
AngieCaiza27 <acaiza0477@uta.edu.ec>
2025-10-06 15:32:37 -0500

    Implementacion de la parte de Categorias para proveedores

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 // app.use('/api/usuarios', require('./routes/usuarios.routes'));
 // app.use('/api/productos', require('./routes/productos.routes'));
 // app.use('/api/proveedores', require('./routes/proveedores.routes'));
-// app.use('/api/categorias', require('./routes/categorias.routes'));
+app.use('/api/categorias', require('./routes/categorias.routes'));
 // app.use('/api/ventas', require('./routes/ventas.routes'));
 // app.use('/api/reportes', require('./routes/reportes.routes'));
 
