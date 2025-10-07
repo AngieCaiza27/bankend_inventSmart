6bcce4db98392e78288160df1be7939023fc8123
AngieCaiza27 <acaiza0477@uta.edu.ec>
2025-10-06 22:18:39 -0500

    Implementacion de endpoints de provedores y arreglos en la base de datos

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
  app.use('/api/auth', require('./routes/auth.routes'));
 // app.use('/api/usuarios', require('./routes/usuarios.routes'));
 // app.use('/api/productos', require('./routes/productos.routes'));
-// app.use('/api/proveedores', require('./routes/proveedores.routes'));
+app.use('/api/proveedores', require('./routes/proveedor.routes'));
 app.use('/api/categorias', require('./routes/categorias.routes'));
 // app.use('/api/ventas', require('./routes/ventas.routes'));
 // app.use('/api/reportes', require('./routes/reportes.routes'));
